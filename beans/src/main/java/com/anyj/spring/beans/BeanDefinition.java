9604a8c3e4ecae6ba5c4fccc3a6d8fe66e2a487a
Yongjie An <anyj1024@qq.com>
2024-09-27 12:23:12 +0800

    feat: BeanDefinition & BeanFactory

---
+++ beans/src/main/java/com/anyj/spring/beans/BeanDefinition.java
@@ -0,0 +1,14 @@
+package com.anyj.spring.beans;
+
+public class BeanDefinition {
+
+    private Object bean;
+
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+}
